15a391c2ce61aea91ac7e749faef79a6d4bfb810
christinekarimi <karimikim3@gmail.com>
2020-10-15 12:50:11 +0300

    add constructor function for addresses and reset fields function

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,24 @@ function Contact(firstName, lastName) {
     this.addresses = [];
   }
   
+  Contact.prototype.fullName = function(){
+    return this.firstName + " " + this.lastName;
+  }
+  
+  function Address(street, city, state) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+  }
+  Address.prototype.fullAddress = function() {
+    return this.street + ", " + this.city + ", " + this.state;
+  }
+  
+  function resetFields() {
+    $("input#new-first-name").val("");
+     $("input#new-last-name").val("");
+     $("input.new-street").val("");
+     $("input.new-city").val("");
+     $("input.new-state").val("");
+    //  $("div.new-address").not(':first').remove();
+   }  
