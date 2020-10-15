75034234a266adb0c799fa2d80d5202b64626f93
christinekarimi <karimikim3@gmail.com>
2020-10-15 12:52:07 +0300

    add jquery function to append new addresses after the the submit button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,65 @@ function Contact(firstName, lastName) {
      $("input.new-state").val("");
     //  $("div.new-address").not(':first').remove();
    }  
+
+   $(document).ready(function() {
+    $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+  
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+      debugger;
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+  
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $(".new-address").each(function() {
+       var inputtedStreet = $(this).find("input.new-street").val();
+       var inputtedCity = $(this).find("input.new-city").val();
+       var inputtedState = $(this).find("input.new-state").val();
+  
+       var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+       newContact.addresses.push(newAddress);
+  
+     });
+  
+     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+  
+        $(".contact").last().click(function() {
+          $("#show-contact").show();
+  
+          $("#show-contact h2").text(newContact.firstName);
+          $(".first-name").text(newContact.firstName);
+          $(".last-name").text(newContact.lastName);
+  
+          $("ul#addresses").text("");
+          newContact.addresses.forEach(function(address) {
+            $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+          });
+        });
+        resetFields();
+        // $("input#new-first-name").val(""); //refactored by brining this code into a function
+        // $("input#new-last-name").val("");  //outside of the event binding, preprocess it before
+        // $("input.new-street").val("");     //document loads. now we can reference the reset field function
+        // $("input.new-city").val("");
+        // $("input.new-state").val("");
+  
+      });
+    });
+  
